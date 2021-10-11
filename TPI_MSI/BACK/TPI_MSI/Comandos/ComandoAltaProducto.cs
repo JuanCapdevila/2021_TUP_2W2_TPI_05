6af7062515590a48bb76ad7bf6748906c47d18a0
1w3112089CardellinoFernandoAgustin <69600638+1w3112089CardellinoFernandoAgustin@users.noreply.github.com>
2021-10-11 16:20:49 -0300

    se suman controladores

---
+++ TPI_MSI/BACK/TPI_MSI/Comandos/ComandoAltaProducto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Comandos.ComandoAltaProducto {
+
+    public class ComandoAltaProducto {
+        public int IdRubro {get; set;}
+        public int IdMarca {get; set;}
+        public int IdEmpaquetado {get; set;}
+    }
+
+}
